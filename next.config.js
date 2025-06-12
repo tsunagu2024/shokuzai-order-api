fc61d0ea156649c35fd132e8c5a9d2f4ff7e3636
tsunagu2024 <tsunagu.agri2024@gmail.com>
2025-06-12 10:53:35 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'standalone',
+  experimental: { appDir: true }
 }
 
 module.exports = nextConfig
